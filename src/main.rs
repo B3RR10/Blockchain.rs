fa39490b466b0020c1b3d44e7156894538fc0bd8
dvogt23 <divogt@live.de>
2017-09-13 06:47:02 +0200

    sha256 function test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate ring;
+
+use ring::digest::{digest, Algorithm, SHA256};
+
 fn main() {
-    println!("Hello, world!");
+    let s: Vec<u8> = vec![1, 2, 3];
+    let x = digest(&SHA256, &s);
+    println!("{:?}", x);
+}
+
+#[test]
+fn main_test() {
+    assert!(true);
+    assert!(false);
 }
